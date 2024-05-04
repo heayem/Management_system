ed74d7f87d879735c276d00b15cb00fb7327cbc5
heayem <heayem7@gmail.com>
2024-05-05 03:03:57 +0700

    task: create dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Department;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $this->authorizeAdministrator();
+
+        $totalUsers = User::count();
+        $totalDepartments = Department::count();
+
+        return Inertia::render('AdminPage/Dashboard', ['totalUsers' => $totalUsers, 'totalDepartments' => $totalDepartments]);
+    }
+
+
+    private function authorizeAdministrator()
+    {
+        if (!Auth::user()->hasRole('administrator')) {
+            abort(403, 'Unauthorized action. Only administrators can access this resource.');
+        }
+    }
+}
